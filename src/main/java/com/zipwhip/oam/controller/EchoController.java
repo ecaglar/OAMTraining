6e5bb9b483f3a8d298f8c678aab1244f97488d4d
Charles Horsman <chorsman@zipwhip-admins-MacBook-Pro-35.local>
2020-03-25 21:58:03 -0700

    Cleared OEM of all training updates to reset to clean state for Twitter. Creating branch for tracking

--- src/main/java/com/zipwhip/oam/controller/EchoController.java
+++ src/main/java/com/zipwhip/oam/controller/EchoController.java
@@ -2,9 +2,9 @@ package com.zipwhip.oam.controller;
 
 import com.zipwhip.oam.service.EchoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.lang.reflect.Member;
 
 
 @RestController
@@ -17,4 +17,7 @@ public class EchoController {
     public String helloUser(@PathVariable("name") final String name){
         return echoService.doWelcome(name);
     }
+
+
+
 }
--- src/main/java/com/zipwhip/oam/repository/EchoRepository.java
+++ src/main/java/com/zipwhip/oam/repository/EchoRepository.java
@@ -9,4 +9,5 @@ public class EchoRepository {
         return "Hello " + name;
     }
 
+
 }
